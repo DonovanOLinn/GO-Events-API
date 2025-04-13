62d05893ed135e2f566371c8d7cc819d852afce0
DonovanOLinn <donovan.b.olinn@gmail.com>
2025-04-13 12:36:07 -0500

    adding DB, updating save & get events for db use

--- models/event.go
+++ models/event.go
@@ -1,9 +1,14 @@
 package models
 
-import "time"
+import (
+	"fmt"
+	"time"
+
+	"example.com/api/db"
+)
 
 type Event struct {
-	ID          int
+	ID          int64
 	Name        string `binding:"required"`
 	Description string `binding:"required"`
 	Location    string `binding:"required"`
@@ -11,13 +16,49 @@ type Event struct {
 	UserID 		int 
 }
 
-var events = []Event{}
+// var events = []Event{}
 
-func (e Event) Save() {
-	// later: add it to a database
-	events = append(events, e)
+func (e Event) Save() error {
+	query := `
+	INSERT INTO events(name, description, location, dateTime, user_id)
+	VALUES (?, ?, ?, ?, ?) 
+	`
+	// Prepare() prepares a SQL statement to execute
+	stmt, err := db.DB.Prepare(query)
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+	fmt.Println(e.Name, e.Description, e.Location, e.DateTime, e.UserID)
+	result, err := stmt.Exec(e.Name, e.Description, e.Location, e.DateTime, e.UserID)
+	if err != nil {
+		return err
+	}
+	id, err := result.LastInsertId()
+	e.ID = id
+	return err
 }
 
-func GetAllEvents() []Event {
-	return events
+func GetAllEvents() ([]Event, error) {
+	query := "SELECT * FROM events"
+	rows, err := db.DB.Query(query)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var events []Event
+
+	for rows.Next() {
+		var event Event
+		err := rows.Scan(&event.ID, &event.Name, &event.Description, &event.Location, &event.DateTime, &event.UserID)
+
+		if err != nil {
+			return nil, err
+		}
+
+		events = append(events, event)
+	}
+
+	return events, nil
 }
